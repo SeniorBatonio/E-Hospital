20b00c56b530da149e3686167743c7f7e65ae4c2
Олексій Рудь <opupep06@gmail.com>
2019-10-27 01:38:30 +0300

    Add AppointmentService

---
+++ E-Hospital.Domain/Interfaces/IAppointmentService.cs
@@ -0,0 +1,15 @@
+﻿using E_Hospital.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Hospital.Domain.Interfaces
+{
+    interface IAppointmentService
+    {
+        Appointment Create(int reservationId, int patientId);
+        void Remove(Appointment appointment);
+    }
+}
---
+++ E-Hospital.Domain/Services/AppointmentService.cs
@@ -0,0 +1,36 @@
+﻿using E_Hospital.Data.Entity;
+using E_Hospital.Data.Repositories;
+using E_Hospital.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Hospital.Domain.Services
+{
+    class AppointmentService : IAppointmentService
+    {
+        private IAppointmentRepository _appointmentRepo;
+        public AppointmentService(IAppointmentRepository appointmentRepo)
+        {
+            _appointmentRepo = appointmentRepo;
+        }
+        public Appointment Create(int reservationId, int patientId)
+        {
+
+            var newAppointment = new Appointment
+            {
+                ReservationId = reservationId,
+                PatientId = patientId
+            };
+            _appointmentRepo.Create(newAppointment);
+            return newAppointment;
+        }
+
+        public void Remove(Appointment appointment)
+        {
+            _appointmentRepo.Delete(appointment);
+        }
+    }
+}
